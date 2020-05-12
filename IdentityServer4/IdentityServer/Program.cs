4e8466cd52ca4fb37e5d183846a759aa103ff208
Jeremiah Flaga <j.flaga@arcanys.com>
2020-05-12 21:35:20 +0800

    renamed IdentityServer project; and initial changes in Startup class

--- IdentityServer4/IdentityServer/Program.cs
+++ IdentityServer4/IdentityServer/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace IdentityServer4
+namespace IdentityServer
 {
     public class Program
     {
