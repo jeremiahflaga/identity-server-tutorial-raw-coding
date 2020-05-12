61177c1872537c8843e08830035e58fdd5c2c659
Jeremiah Flaga <flaga.jeremiah@gmail.com>
2020-05-13 04:12:49 +0800

    configured Startup class of ApiOne client project

--- IdentityServer4/ApiOne/Startup.cs
+++ IdentityServer4/ApiOne/Startup.cs
@@ -16,6 +16,7 @@ namespace ApiOne
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers(); // [video-11 14:30] no views
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,12 +29,12 @@ namespace ApiOne
 
             app.UseRouting();
 
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
