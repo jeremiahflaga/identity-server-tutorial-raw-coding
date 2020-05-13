ba35e5d9852894d50a2ad2cf961ea2e895e64641
Jeremiah Flaga <flaga.jeremiah@gmail.com>
2020-05-13 08:09:43 +0800

    setup Startup class in ApiTwo (mostly copied from ApiOne's Startup class, except config.Audience)

--- IdentityServer4/ApiTwo/Startup.cs
+++ IdentityServer4/ApiTwo/Startup.cs
@@ -8,17 +8,22 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace ApiTwo
+namespace ApiOne
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer("Bearer", config =>
+                {
+                    config.Authority = "https://localhost:44375/";
+                    config.Audience = "ApiTwo";
+                });
+
+            services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -28,12 +33,12 @@ namespace ApiTwo
 
             app.UseRouting();
 
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
